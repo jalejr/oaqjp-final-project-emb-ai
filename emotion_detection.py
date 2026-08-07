02598bf4dd6eff042a0fbd19b69ea01369c653c6
jalejr <jalejr.dev@gmail.com>
2026-08-07 18:42:29 -0400

    task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,4 +8,8 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=header)
 
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+    emotions["dominant_emotion"] = max(emotions, key=emotions.get)
+
+    return emotions
