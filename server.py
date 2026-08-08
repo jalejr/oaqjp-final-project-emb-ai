db2d7af1c36d76c6377f2156b1ced11bda4e3794
jalejr <jalejr.dev@gmail.com>
2026-08-07 20:22:22 -0400

    task 8

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request 
+"""Server that runs emotion detection."""
+from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
-@app.route("/emotionDetector") 
-def sent_analyzer(): # Retrieve the text to analyze from the request arguments 
+@app.route("/emotionDetector")
+def sent_detector():
+    """Route for the emotion detector with logic."""
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
@@ -15,11 +17,18 @@ def sent_analyzer(): # Retrieve the text to analyze from the request arguments
     sadness = response["sadness"]
     dominant = response["dominant_emotion"]
 
-    return f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant}."
+    if dominant is None:
+        return "Invalid text! Please try again!"
+
+    return (f"For the given statement, the system response is 'anger': {anger}, "
+            f"'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': "
+            f"{sadness}. The dominant emotion is {dominant}."
+        )
 
 
 @app.route("/")
 def render_index_page():
+    """Route for default page render."""
     return render_template('index.html')
 
 
